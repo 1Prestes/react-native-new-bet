9ebde71c23e5e10131b77e0d028d67c3bd152259
Dijango Alves Rodolfo <dijango_ilha@hotmail.com>
2021-05-18 16:27:16 -0300

    chore: add git message linter

--- App.tsx
+++ App.tsx
@@ -1,14 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
 
-export default function App() {
+export default function App () {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View>
+        <Text style={styles.logo}>TGL</Text>
+      </View>
+      <StatusBar style='auto' />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
@@ -16,6 +18,9 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
   },
-});
+  logo: {
+    fontSize: 44
+  }
+})
